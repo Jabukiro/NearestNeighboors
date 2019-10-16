8a7b2b5c16c9f70e6330d895cb43a370a733b384
jabukiro <d.barihuta@gmail.com>
2019-10-16 12:24:06 +0800

    Add loading screen

---
+++ loading.lua
@@ -0,0 +1,25 @@
+-- ---------------
+-- loading.lua
+-- ---------------
+
+local composer = require( "composer" )
+
+local scene = composer.newScene()
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+-- Preload the data here
+local sceneNumber = 0
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
